cb39f03def276ac16a9cef3806d443177756137d
JuanContreras <mizamarzes@gmail.com>
2024-02-13 13:21:31 -0500

    aksdllask

--- js/main.js
+++ js/main.js
@@ -1,46 +1,98 @@
-const numberVerify = function () {
-    let listVerifyNumber = JSON.parse(localStorage.getItem('listVerifyNumber')) || [];
-    userNumber = document.getElementById("user-number").value;
-    //reverse the text
-    userNumber = userNumber.split("").reverse("").join("");
-    let i = 2;
-    let totSum = 0;
-    for(let number of userNumber){// of give the value in give the key
-        totSum += parseInt(number)*i;
-        i++; 
-        if(i > 7){
-            i = 2;
-        }
-    }
-    totSum %= 11;
-    totSum = 11- totSum;
-    userNumber = userNumber.split("").reverse("").join("");
-    let result = userNumber+"-"+totSum.toString();
-    let numberFound = false;
-    for(let number of listVerifyNumber){
-        if(number === result ){
-            result = "Already in the database"
-            numberFound = true;
-        }
-    }
-    if(!numberFound){
-        listVerifyNumber.push(result);
-        localStorage.setItem('listVerifyNumber', JSON.stringify(listVerifyNumber));
-    }
-    document.getElementById("result-verify").innerHTML="Result: "+result;
-    event.preventDefault();
-};
+const iniciar = function () {
+    let lista = ["201012341-3"]
+    localStorage.setItem("data", JSON.stringify(lista));
+}
 
-function login(){
-    let listVerifyNumber = JSON.parse(localStorage.getItem('listVerifyNumber')) || [];
-    userNumber = document.getElementById("user-number").value;
-    let result = "Number not in the database";
-    for(let number of listVerifyNumber){
-        if(number == userNumber){
-            result = "Login succesfull";
-            break
-        }
+const listados = function () {
+    var data = JSON.parse(localStorage.getItem("data"));
+    let boton = document.getElementById("button3").textContent
+
+    if(boton == "Show"){
+        document.getElementById("listado").innerHTML = data;
+        document.getElementById("button3").innerHTML = "Hide";
+    }else{
+        document.getElementById("button3").innerHTML = "Show";
+        document.getElementById("listado").innerHTML = "Click to display";
     }
-    document.getElementById("result-verify").innerHTML="Result: "+result;
-    event.preventDefault();
+}
+
+const repetido = function () {
+    let numberVerificador = (document.getElementById("number-vef").value);
+    var data = JSON.parse(localStorage.getItem("data"));
+
+    let td = data.length;
+    let invertida = "";
+    let multi = "";
+    let modulo = 0;
+    let lista = []
+    lista = data;
+
+
+    invertida = (invertirCadena(numberVerificador));
+
+    multi = (multiplicar(invertida));
+    modulo = multi % 11;
+    resultado = 11 - modulo;
+    console.log(resultado);
+    let pos = lista.includes(numberVerificador + "-" + resultado)
+    console.log(pos)
+    if (pos == true) {
+        alert("the number is repeated")
+    }
+    else {
+        alert("the number is not repeated")
+    }
+}
+const verificar = function () {
+    let numberVerificador = (document.getElementById("number-vef").value);
+    var data = JSON.parse(localStorage.getItem("data"));
+
+    let td = data.length;
+    let invertida = "";
+    let multi = "";
+    let modulo = 0;
+    let lista = []
+    lista = data;
+
+    invertida = (invertirCadena(numberVerificador));
+
+    multi = (multiplicar(invertida));
+    modulo = multi % 11;
+    resultado = 11 - modulo;
+    console.log(resultado);
+    let pos = lista.includes(numberVerificador + "-" + resultado)
+    console.log(pos)
+    if (pos == true) {
+        console.log("repetido");
+        alert("el numero esta repetido")
+    }
+    else {
+        document.getElementById("resultado").innerHTML = numberVerificador + "-" + resultado;
+        lista[td] = numberVerificador + "-" + resultado;
+        console.log(data);
+
+    }
+    localStorage.setItem("data", JSON.stringify(lista));
+}
+
+function invertirCadena(cad) {
+    var separarCadena = cad.split("");
+    var invertirArreglo = separarCadena.reverse();
+    var unirArreglo = invertirArreglo.join("");
+    return unirArreglo;
+}
+function multiplicar(cad) {
+    let n = 0;
+    let j = 1;
+    let suma = 0;
+    var separarCadena = cad.split("");
+    n = separarCadena.length
+    for (let i = 0; i <= n - 1; i++) {
+        j = j + 1;
+        if (j > 7) {
+            j = j - 6;
+        }
+        suma = parseFloat(cad[i]) * j + suma;
+    }
+    return suma;
 }
